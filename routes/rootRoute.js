f5afca3876e4d31a8b36fba71d021f18581366f6
Madhav Pandey <pmadhav279@gmail.com>
2024-08-13 22:39:07 +1000

    robots.txt and sitemap.xml added for seo

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -3,14 +3,13 @@ const Posts = require("../module/posts");
 const rootRoute = require("express").Router();
 const Comment = require("../module/comment");
 
-
 // API to get blogs
 rootRoute.get("/api/:email", async (req, res) => {
   let email = req.params.email;
 
   try {
     let user = await Users.findOne({ email });
-    let blogs = await Posts.find({ uid: user._id })
+    let blogs = await Posts.find({ uid: user._id, status: "published" })
       .populate({ path: "uid", select: "_id name profileURL" })
       .sort({ _id: -1 })
       .lean();
@@ -20,23 +19,18 @@ rootRoute.get("/api/:email", async (req, res) => {
   }
 });
 
-
-
-
 // A user posts
 
 rootRoute.get("/user", async (req, res) => {
-  const {id} = req.query;
+  const { id } = req.query;
 
-  if(req.session.uid === id){
+  if (req.session.uid === id) {
     return res.redirect("/welcome");
-  } 
+  }
   try {
-    const user = await Users.findById({ _id: id })
-      .select("-password")
-      .lean();
+    const user = await Users.findById({ _id: id }).select("-password").lean();
 
-    const posts = await Posts.find({ uid: id })
+    const posts = await Posts.find({ uid: id, status: "published" })
       .populate({ path: "uid", select: "_id name profileURL" })
       .sort({ _id: -1 })
       .select("_id title createdAt html")
@@ -52,7 +46,6 @@ rootRoute.get("/user", async (req, res) => {
   }
 });
 
-
 // Getting a single post
 rootRoute.get("/:postId", async (req, res) => {
   const { postId } = req.params;
@@ -67,25 +60,22 @@ rootRoute.get("/:postId", async (req, res) => {
       .populate({ path: "replies" })
       .populate({ path: "postedBy", select: "-password" })
       .lean();
-      if(!post) return res.render("404")
-    
-      res.render("singlePost", {
-        post,
-        comments,
-      });
-    
+    if (!post) return res.render("404");
+
+    res.render("singlePost", {
+      post,
+      comments,
+    });
   } catch (er) {
-    res.render("404")
+    res.render("404");
   }
 });
 
-
-
 rootRoute.get("/", async (req, res) => {
   try {
-//  let statusFieldAddedSuccessfully = await Posts.updateMany({status:{$exists:false}}, {$set:{status: "published"}});
-//     console.log(statusFieldAddedSuccessfully);
-    const posts = await Posts.find()
+    //  let statusFieldAddedSuccessfully = await Posts.updateMany({status:{$exists:false}}, {$set:{status: "published"}});
+    //     console.log(statusFieldAddedSuccessfully);
+    const posts = await Posts.find({ status: "published" })
       .sort({ _id: -1 })
       .populate({ path: "uid", select: "_id name profileURL" })
       .select("_id title createdAt html")
@@ -95,7 +85,7 @@ rootRoute.get("/", async (req, res) => {
     });
   } catch (err) {
     console.error(err);
-    res.json({message: err.message});
+    res.json({ message: err.message });
   }
 });
 
