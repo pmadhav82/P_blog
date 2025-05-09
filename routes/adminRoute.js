786f20064507f3c1036de404a20c4bd371e1e17e
Madhav Pandey <pmadhav279@gmail.com>
2025-05-09 22:28:07 +1000

    media can delete

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,36 +1,53 @@
 const adminRoute = require("express").Router();
 const Users = require("../module/user");
 const client = require("../utils/s3Client");
-const {ListObjectsV2Command} = require("@aws-sdk/client-s3");
+const { ListObjectsV2Command, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 
-adminRoute.get("/", async (req,res)=>{
+const BUCKET_NAME = "pblog-images-bucket";
 
-const users = await Users.find().select("-password").lean();
+adminRoute.get("/", async (req, res) => {
+  const users = await Users.find().select("-password").lean();
+
+  res.render("admin", { users });
+});
+
+adminRoute.get("/media", async (req, res) => {
+  const command = new ListObjectsV2Command({ Bucket: BUCKET_NAME });
+  try {
+    const response = await client.send(command);
+    const imageKey = response.Contents?.map(
+      (obj) => obj.Key
+    );
 
 
-    res.render("admin",{users})
-})
-
-
-adminRoute.get("/media", async(req,res)=>{
-const command = new ListObjectsV2Command({Bucket: "pblog-images-bucket"});
-try{
-const response = await client.send(command);
-const imageURL = response.Contents?.map(obj=>`https://pblog-images-bucket.s3.ap-southeast-2.amazonaws.com/${obj.Key}`);
-
-console.log(imageURL);
-res.render("media",{
-    imageURL
-})
-}catch(error){
+    res.render("media", {
+      imageKey
+    });
+  } catch (error) {
     console.log(error.message);
     res.redirect("/admin");
+  }
+});
+
+
+
+
+adminRoute.post("/media/:imageKey", async(req,res)=>{
+
+const {imageKey} = req.params;
+
+const command = new DeleteObjectCommand({Bucket:BUCKET_NAME, Key:imageKey});
+
+try{
+await client.send(command);
+
+}catch(error){
+    console.log(error.message);
+    res.redirect("/admin/media")
 }
 
-
-   
+    res.redirect("/admin/media")
 })
-
 
 
 module.exports = adminRoute;
